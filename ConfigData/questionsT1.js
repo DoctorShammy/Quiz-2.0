d7c5b5683a4714a61441374bd5e4733ac89bd322
Vera Krapivner <vera.krapivner@iths.se>
2023-11-16 14:44:16 +0100

    lagt till logga

--- ConfigData/questionsT1.js
+++ ConfigData/questionsT1.js
@@ -14,7 +14,7 @@ const quizQuestions = [
     },
     {
         id: 'question2',
-        text: 'What should you look for in a customer? (tick all that apply)',
+        text: '2. What should you look for in a customer? (tick all that apply)',
         options: [
           { id: '2a', text: 'Big portfolio with small average size of buildings' },
           { id: '2b', text: 'Big average size of buildings in a portfolio' },
@@ -26,19 +26,19 @@ const quizQuestions = [
       },
       {
         id: 'question3',
-        text: 'What attributes are important when choosing what building to optimize? (tick all that apply)',
+        text: '3. What attributes are important when choosing what building to optimize? (tick all that apply)',
         options: [
           { id: '3a', text: 'High number of technical systems' },
           { id: '3b', text: 'Buildings with low number of technical systems          ' },
           { id: '3c', text: 'Buildings with an excessively large proportion of VAV (variable air volume)          ' },
           { id: '3d', text: 'Buildings with varied use' }
       ],
-        correctAnswer: ['3a', '3d' ],
+        correctAnswer:['3a', '3d' ],
         maxChoices: 2
       },
       {
         id: 'question4',
-        text: 'Which buildings are the optimal to optimize (tier 1)? (Tick all that apply)',
+        text: '4. Which buildings are the optimal to optimize (tier 1)? (Tick all that apply)',
         options: [
           { id: '4a', text: 'Offices' },
           { id: '4b', text: 'Shops' },
@@ -50,7 +50,7 @@ const quizQuestions = [
       },      
       {
         id: 'question5',
-        text: 'Which buildings need more investigation before proceeding? (tick all that apply)        ',
+        text: '5. Which buildings need more investigation before proceeding? (tick all that apply)        ',
         options: [
           { id: '5a', text: 'Shopping malls' },
           { id: '5b', text: 'Train stations' },
@@ -62,7 +62,7 @@ const quizQuestions = [
       },      
       {
         id: 'question6',
-        text: 'Which type of building should you avoid? (tick all that apply)        ',
+        text: '6. Which type of building should you avoid? (tick all that apply)        ',
         options: [
           { id: '6a', text: 'Elderly care facilities' },
           { id: '6b', text: 'Schools' },
@@ -74,14 +74,14 @@ const quizQuestions = [
       },      
       {
         id: 'question7',
-        text: 'What attributes should you avoid? (tick all that apply)',
+        text: '7. What attributes should you avoid? (tick all that apply)',
         options: [
           { id: '7a', text: 'Building with specific requirements on the indoor climate, like ice rinks or swimming pools' },
           { id: '7b', text: 'Buildings with an excessively large proportion of VAV (variable air volume)' },
           { id: '7c', text: 'Small individual buildings in a portfolio' },
           { id: '7d', text: 'Military facilities' }
       ],
-        correctAnswer: ['7a', '7b','7c', '7d' ],
+        correctAnswer: ['7a','7b', '7c','7d', ],
         maxChoices: 4
       },
       ]
--- Logic/testButtons.js
+++ Logic/testButtons.js
@@ -1,5 +1,6 @@
 function showTestPage() {
-    window.location.href = 'testpage.html'; 
+    //window.location.href = 'testpage.html'; 
+    window.location.href = 'testpage2.html'; 
 
 }
 function showSchneiderTestPage() {
